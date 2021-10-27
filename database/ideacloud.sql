e630848ac0c3d9ec277cdf7e6dcccffd792087f1
lehoang0812 <hoangoku@gmail.com>
2021-10-27 18:09:10 +0700

    sửa lỗi db, thêm db, phác hoạ ít gd manage user

--- database/ideacloud.sql
+++ database/ideacloud.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 26, 2021 at 03:59 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 27, 2021 lúc 01:06 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ideacloud`
+-- Cơ sở dữ liệu: `ideacloud`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `docs`
+-- Cấu trúc bảng cho bảng `docs`
 --
 
 CREATE TABLE `docs` (
@@ -33,17 +33,26 @@ CREATE TABLE `docs` (
   `doc_author` varchar(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   `doc_date` date DEFAULT NULL,
   `description` varchar(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `visibitity` int(11) NOT NULL,
+  `visibility` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
   `type_file` char(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
   `filename` varchar(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   `user_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `docs`
+--
+
+INSERT INTO `docs` (`doc_ID`, `doc_name`, `doc_author`, `doc_date`, `description`, `visibility`, `type_file`, `type`, `filename`, `user_ID`) VALUES
+(1, 'Tài liệu 1', 'Tác giả 1', '2001-08-21', 'Mô tả 1, test', 'Only Me', NULL, NULL, 'test.sql', 3),
+(2, 'Tài liệu 2', 'Tác giả 2', '2001-08-21', 'Mô tả 2, test', 'Public', NULL, NULL, 'test2.sql', 4),
+(3, 'Tài liệu 3', 'Tác giả 3', '2001-08-21', 'Mô tả 3, test', 'Public', NULL, NULL, 'test3.sql', 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `doc_groups`
+-- Cấu trúc bảng cho bảng `doc_groups`
 --
 
 CREATE TABLE `doc_groups` (
@@ -51,10 +60,19 @@ CREATE TABLE `doc_groups` (
   `group_name` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `doc_groups`
+--
+
+INSERT INTO `doc_groups` (`group_ID`, `group_name`) VALUES
+(1, 'Excel'),
+(2, 'Docx'),
+(3, 'Ppt');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group_detail`
+-- Cấu trúc bảng cho bảng `group_detail`
 --
 
 CREATE TABLE `group_detail` (
@@ -62,87 +80,107 @@ CREATE TABLE `group_detail` (
   `group_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `group_detail`
+--
+
+INSERT INTO `group_detail` (`doc_ID`, `group_ID`) VALUES
+(1, 3),
+(1, 2),
+(2, 3),
+(3, 2);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL CHECK (`email` REGEXP '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$'),
-  `SDT` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL CHECK (`SDT` REGEXP '0[35789]\\d{8}$'),
-  `status` tinyint(1) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL CHECK (`email` regexp '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$'),
+  `SDT` int(13) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
   `code` varchar(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `authorize` tinyint(1) DEFAULT NULL
+  `authorize` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Indexes for dumped tables
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `SDT`, `status`, `code`, `authorize`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', 1234564, 1, NULL, 1),
+(2, 'test', '123', 'test@gmail.com', 1234567, 0, NULL, 0),
+(3, 'user1', '123', 'user1@gmail.com', 965132132, 1, NULL, 0),
+(4, 'user2', '123', 'user2@gmail.com', 541652164, 1, NULL, 0);
+
+--
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `docs`
+-- Chỉ mục cho bảng `docs`
 --
 ALTER TABLE `docs`
   ADD PRIMARY KEY (`doc_ID`),
   ADD KEY `user_ID` (`user_ID`);
 
 --
--- Indexes for table `doc_groups`
+-- Chỉ mục cho bảng `doc_groups`
 --
 ALTER TABLE `doc_groups`
   ADD PRIMARY KEY (`group_ID`);
 
 --
--- Indexes for table `group_detail`
+-- Chỉ mục cho bảng `group_detail`
 --
 ALTER TABLE `group_detail`
   ADD KEY `doc_ID` (`doc_ID`),
   ADD KEY `group_ID` (`group_ID`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `docs`
+-- AUTO_INCREMENT cho bảng `docs`
 --
 ALTER TABLE `docs`
-  MODIFY `doc_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `doc_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `doc_groups`
+-- AUTO_INCREMENT cho bảng `doc_groups`
 --
 ALTER TABLE `doc_groups`
-  MODIFY `group_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `group_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `docs`
+-- Các ràng buộc cho bảng `docs`
 --
 ALTER TABLE `docs`
   ADD CONSTRAINT `docs_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_id`);
 
 --
--- Constraints for table `group_detail`
+-- Các ràng buộc cho bảng `group_detail`
 --
 ALTER TABLE `group_detail`
   ADD CONSTRAINT `group_detail_ibfk_1` FOREIGN KEY (`doc_ID`) REFERENCES `docs` (`doc_ID`),
---
+++ src/admin/MnUser.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+    <title>Login Admin</title>
+</head>
+
+<body>
+<?php 
+            if(isset($_SESSION['login']))
+            {
+               header('location:login.php');
+            }
+     ?>
+<main>
+<!-- Page Content -->
+<div class="container">
+  <div class="card border-0 shadow my-5">
+    <div class="card-body p-5">
+      <h1 class="fw-light">Fixed Full Page Background Image</h1>
+      <p class="lead">In this snippet, the background image is fixed to the body element. Content on the page will
+        scroll, but the image will remain in a fixed position!</p>
+      <p class="lead">Scroll down...</p>
+      <div style="height: 700px"></div>
+      <p class="lead mb-0">You've reached the end!</p>
+    </div>
+  </div>
+</div>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
